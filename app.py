4d1cd633b423553e41eadc1cdeb239f70aafa7e1
Tech VJ <122880036+VJBots@users.noreply.github.com>
2023-10-05 17:21:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,7 @@ def hello_world():
 
 if __name__ == "__main__":
     app.run()
+
+# Don't Remove Credit @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
+# Ask Doubt on telegram @KingVJ01
