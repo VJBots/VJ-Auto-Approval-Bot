84e1af61a805998cc3d8dd1a8c512bac728d55b2
ROHITT001 <157211770+ROHITT001@users.noreply.github.com>
2024-01-21 08:03:59 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,16 +6,13 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", "20389440"))
-    API_HASH = getenv("API_HASH", "a1a06a18eb9153e9dbd447cfd5da2457")
-    BOT_TOKEN = getenv("BOT_TOKEN", "6564513574:AAGDqUaEmeu0m4DjLDetNc4nooVTWYT7Fzo")
-    FSUB = getenv("FSUB", "VJ_Botz")
-    CHID = int(getenv("CHID", "-1001623633000"))
-    SUDO = list(map(int, getenv("SUDO", "6168162777").split()))
-    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://sushankm16:4i1WAfPYKWyqPIDD@cluster0.sngp9pz.mongodb.net/?retryWrites=true&w=majority")
+    API_ID = int(getenv("API_ID", "28421635"))
+    API_HASH = getenv("API_HASH", "a4856de5fec0b9b3601ff06425f3f58e)
+    BOT_TOKEN = getenv("BOT_TOKEN", "6947074831:AAHfx5XvS6p0BXjamh3mdIKtjaC6MMgM4x0")
+    FSUB = getenv("FSUB", "TeamAnimePedia")
+    CHID = int(getenv("CHID", "-1001991806323)
+    SUDO = list(map(int, getenv("SUDO", "6657660775").split()))
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://Filebot:Filebot@cluster0.1kdt8x3.mongodb.net/?retryWrites=true&w=majority")
     
 cfg = Config()
 
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
